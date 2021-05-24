c3ca26ebc9f6e42d94b88c30fbfa2fdeac58a462
AJADHAV5066 <arjunjadhav12329@gmail.com>
2021-05-24 16:25:46 +0530

    Pro-1

---
+++ src/Sample.js
@@ -0,0 +1,85 @@
+
+import './Sample.css'
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+
+function Sample() {
+   
+  return (
+    
+      <div>
+        
+        <div className="Sample0">
+      
+      <p>
+      <div><img src="/image/Untitled-1.jpg"alt=""/></div>
+       </p>
+       
+         
+  
+    
+</div>
+     <div className="Sample1">
+      
+      <p>Left Shopping Window</p>
+       <div className="Sample5">
+
+       <r>12 Accessories</r>
+    </div>
+       <h6><button color=""class="btn btn-outline-primary">ON</button></h6>
+       </div>
+       
+          
+  
+    
+
+   
+      <div className="Sample2">
+        
+<p>
+       Right Shopping Window
+     </p>
+     <div className="Sample6">
+     <r>9 Accessories</r></div>
+     <h6><button type="button" className="buttonliston">ON</button></h6>
+     
+    
+ 
+    </div>
+        <div className="Sample3">
+        <p>
+          Front Entrance
+          </p>
+          <div className="Sample7">
+          <r>12 Accessories</r></div>
+          <h6><button type="button" className="buttonliston">ON</button></h6>
+        
+
+    </div> 
+
+    <div className="Sample4">
+        <p>
+                 Shoe Wall
+    
+  
+    
+        </p>
+        <div className="Sample8">
+        <r>20 Accessories</r></div>
+        <h6><button type="button" className="buttonliston">ON</button></h6>
+        
+        
+        
+
+    </div> 
+    
+   
+
+
+</div>
+  );
+}
+
+export default Sample;
+
